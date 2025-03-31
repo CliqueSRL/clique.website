83515c648426d10bbf0b4798a0623ebe8fb99651
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2025-03-31 23:34:27 +0200

    Added template websites

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -67,7 +67,7 @@ export default async function ProjectsPage() {
                 </p>
                 <div className="absolute bottom-4 md:bottom-8">
                   <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
-                    Scopri di più<span aria-hidden="true">&rarr;</span>
+                    Scopri di più <span aria-hidden="true">&rarr;</span>
                   </p>
                 </div>
               </article>
