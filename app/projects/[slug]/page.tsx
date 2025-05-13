1e2f93f83431d94f1d2a9678e2efa1240ce629fd
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2025-05-13 13:33:42 +0000

    Temp update

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -6,37 +6,33 @@ import "./mdx.css";
 
 export const revalidate = 60;
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
+type Params = { slug: string };
 
-export async function generateStaticParams(): Promise<Props["params"][]> {
+export async function generateStaticParams(): Promise<Params[]> {
   return allProjects
     .filter((p) => p.published)
-    .map((p) => ({
-      slug: p.slug,
-    }));
+    .map((p) => ({ slug: p.slug }));
 }
 
-export default async function PostPage({ params }: Props) {
-  const slug = params?.slug;
-  const project = allProjects.find((project) => project.slug === slug);
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { slug } = await params;
 
-  if (!project) {
-    notFound();
-  }
+  const project = allProjects.find((p) => p.slug === slug);
+  if (!project) notFound();
 
   const views = {} as any;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
       <Header project={project} views={views} />
-
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
         <Mdx code={project.body.code} />
       </article>
     </div>
   );
 }
+
